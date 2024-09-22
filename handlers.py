09b784ed9c1a81f8ab09fa2cef2c190979ec9685
MO7YW4NG <motywang@gmail.com>
2024-09-23 02:47:52 +0800

    Allow longer audio inputs

--- handlers.py
+++ handlers.py
@@ -1,5 +1,6 @@
 import aiofiles
 from config import line_bot_api, line_bot_api_blob, groq
+import asyncio
 from utils.message_utils import send_carousel_message, send_text_message
 from utils.file_utils import user_data
 from pathlib import Path 
@@ -11,7 +12,7 @@ async def handle_text_message(event):
     # 用戶資料綁定
     if user_data.get(user_id) is None:
         if len(message.split()) < 3:
-            await send_text_message(event, "請先綁定個人資料！依指定格式輸入：<系級> <學號> <姓名>")
+            await send_text_message(event, "請先綁定個人資料！\n依指定格式輸入：<系級> <學號> <姓名>")
             return
         depart, student_id, name = message.split(' ')[0], message.split(' ')[1], message.split(' ')[2]
         if not student_id.isdigit():
@@ -26,10 +27,21 @@ async def handle_text_message(event):
 
 async def handle_audio_message(event):
     user_id = event.source.user_id
+    
+    result = await line_bot_api_blob.get_message_content_transcoding_by_message_id(event.message.id)
+    
+    while result.status == 'processing':
+        result = await line_bot_api_blob.get_message_content_transcoding_by_message_id(event.message.id)
+        await asyncio.sleep(1)
+
     message_content = await line_bot_api_blob.get_message_content(event.message.id)
 
     async with aiofiles.open(f"{event.message.id}.m4a", "wb") as f:
         await f.write(message_content)
+    
+    if message_content is None:
+        await send_text_message(event, "無法獲取音訊內容，請稍後再試。")
+        return
     
     # 使用 Whisper 進行音訊轉錄
     transcript = groq.audio.transcriptions.create(
